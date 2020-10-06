0f77174b44195e7045cc59d00606c2c275a30356
ShivaChandra <k.s9908725092@gmail.com>
2020-10-06 22:15:26 +0530

    Adding setup.py file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# gsheet_image_generator
+## Installation
+```
+pip install gsheet_image_generator
+``` 
+## How to use it?
+Open the Terminal and Enter Google Sheet ID:-
+
+
+## License
+
+This Package is licensed under the MIT license.
+See License for details.
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r") as fh:
+  long_description=fh.read() 
+
+setup(
+    name='gsheet_image_generator',
+    version='1.0.0',
+    author='shivachandra',
+    author_email='k.s9908725092@gmail.com',
+    url='https://github.com/shivachandrakante/gsheet_image_generator',
+    description='An awesome package which helps us in creating a graph from Google Sheets and stores them as image',
+    long_description=long_description,
+    long_description_content="text/markdown",
+    keywords='spreadhseets google api v4 wrapper csv pandas image_generator',
+    packages=find_packages(),
+    scripts=['bin/script1','bin/script2'],
+    license='LICENSE.txt',
+    platforms='any',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
+    include_package_data=True,
+    install_requires=
+      [
+        "google-auth-oauthlib==0.4.1",
+        "google-auth-httplib2==0.0.4",
+        "gsheets >= 0.5.1",
+        "numpy",
+        "pandas",
+        "matplotlib",
+        'google-api-python-client<1.8; python_version < "3"',
+        'google-api-python-client; python_version >= "3"',
+        'oauth2client>=1.5.0',
+      ],
+    entry_points=
+      {
+          "console_scripts": 
+          [
+            "gsheet_image_generator=gsheet_image_generator.__main__::main",
+          ]
+      }
+)
