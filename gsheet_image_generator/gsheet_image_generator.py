c6642c7875a81a588c9fee5a7deaec835b45a397
ShivaChandra <k.s9908725092@gmail.com>
2020-10-11 10:11:13 +0530

    modified gsheet_image_generator add inline comments for better understanding

--- gsheet_image_generator/gsheet_image_generator.py
+++ gsheet_image_generator/gsheet_image_generator.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from .gsheet_image import Create_service
 
-
+#Creating a class Image_generator to access it using the Object.
 class Image_generator(object):
+    #init class 
+    #This class is called when we create an object for the class and we have to pass the path of Client secrets as argument. 
     def __init__(self,path):
         self.CLIENT_SECRET_FILE = path
         self.API_SERVICE_NAME = 'drive'
@@ -38,15 +40,15 @@ class Image_generator(object):
         self.cols=self.sheet.iloc[0]
         self.sheet=self.sheet.iloc[1:]
         self.sheet.columns=self.cols
-
+    #This function to selct columns while plotting graph
     def get_column(self,axis):
         print(self.sheet.columns)
         print("Enter the number to select the column for "+ axis +' axis')
         return str(input())
-
+    #This function converts Data type of column in dataframe.
     def convert_dtype(self,column,type_conversion):
         self.sheet[column]=self.sheet[column]=self.sheet[column].astype(type_conversion)
-
+    #This function converts timestamp data type to date time column 
     def convert_timestamp_to_datetime(self):
         print(self.sheet.columns)
         print("Select & Enter column to convert it in to Date time")
@@ -54,28 +56,29 @@ class Image_generator(object):
         self.sheet[column]=self.sheet[column].astype('int')
         self.sheet[column]=self.sheet[column].apply(lambda x:pd.Timestamp(x,unit='s').date())
         self.sheet[column]=pd.to_datetime(self.sheet[column])
-    
+    #This function generate year from date time column
     def generate_year(self):
         print("Enter the column name from which you want to extract year")
         print(self.sheet.columns)
         self.column_name=str(input())
         self.sheet['year']=self.sheet[self.column_name].dt.year
+    #This function generate year from month time column
     def generate_month(self):
         print("Enter the column name from which you want to extract year")
         print(self.sheet.columns)
         self.column_name=str(input())
         self.sheet['month']=self.sheet[self.column_name].dt.month
-
+    #This function generate year from day time column
     def generate_day(self):
         print("Enter the column name from which you want to extract year")
         print(self.sheet.columns)
         self.column_name=str(input())
         self.sheet['day']=self.sheet[self.column_name].dt.day
-
+    #This function performs groupby a column passed as argument
     def groupby_column(self,column):
         self.sheet=self.sheet.groupby(by=[column]).sum()
         self.sheet.reset_index(inplace=True)
-
+    #This function plots line graph by selecting two columns its asks for path to store the image
     def plot_line_graph(self):
         self.sheet_cols=self.sheet.columns
         self.x_axis=self.get_column('X') #Selecting X-axis
@@ -91,7 +94,7 @@ class Image_generator(object):
         self.path_to_store=str(input())
         plt.savefig(self.path_to_store+'/'+self.x_axis.capitalize()+"_vs_"+self.y_axis.capitalize()+"_Line_Graph")       #Saving the Graph as Image
         plt.show()
-
+    #This function plots bar graph by selecting two columns its asks for path to store the image
     def plot_bar_graph(self):
         self.sheet_cols=self.sheet.columns
         self.x_axis=self.get_column('X') #Selecting X-axis
@@ -102,7 +105,7 @@ class Image_generator(object):
         self.path_to_store=str(input())
         plt.savefig(self.path_to_store+'/'+self.x_axis.capitalize()+"_vs_"+self.y_axis.capitalize()+"_Bar_Graph")       #Saving the Graph as Image
         plt.show()
-
+    #This function plots box plot by selecting two columns its asks for path to store the image
     def box_plot(self):
         self.sheet_cols=self.sheet.columns
         self.x_axis=self.get_column('X') #Selecting X-axis
@@ -116,7 +119,7 @@ class Image_generator(object):
         self.path_to_store=str(input())
         plt.savefig(self.path_to_store+'/'+self.x_axis.capitalize()+"_vs_"+self.y_axis.capitalize()+"_Box_plot")       #Saving the Graph as Image
         plt.show()
-
+    #This function plots Scatter plot by selecting two columns its asks for path to store the image
     def scatter_plot(self):
         self.sheet_cols=self.sheet.columns
         self.x_axis=self.get_column('X') #Selecting X-axis
